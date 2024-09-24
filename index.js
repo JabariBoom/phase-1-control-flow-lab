7d34c80aa9ab3df8c25ce6829b1c1ee4e43757eb
jabaringure@gmail.com <jabaringure@gmail.com>
2024-09-24 14:29:05 +0300

    Updates Control Flow

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(numberOfFeet){
   // Write your code here!
+  if(numberOfFeet > 2500){
+    return 'No can do.'
+  } 
+  else if(numberOfFeet > 2000){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if(numberOfFeet >= 400 && numberOfFeet <= 2000){ 
+    return 'That will be twenty bucks.' 
+  }
+  else if(numberOfFeet <= 400){
+    return 'This one is on me!'
+  }
+}
+function ternaryCheckCity(aCity){
+  // Write your code here!
+  return aCity === 'NYC'?'Ok, sounds good.' : 'No go.'
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(charm){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  switch(charm){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
+} 
